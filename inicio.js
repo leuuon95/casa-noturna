f543db568f57ffc345dbcca737adb6531e1e7cd8
Lennon Fonseca <lennonfonsecaa@gmail.com>
2022-06-12 13:26:44 -0300

    adicionando treinamento

---
+++ inicio.js
@@ -0,0 +1,23 @@
+let capacidade_total = 530;
+let cadeiras_disponível = 335;
+let camarote = 55;
+
+if (capacidade_total > 510){
+    console.log ("esgotado");
+} else {
+    console.log ("ingressos disponível");
+}
+
+if (cadeiras_disponível > 315){
+    console.log ("assentos indisponível");
+} else {
+    console.log ("assentos disponível");
+}
+
+if (camarote < 60){
+    console.log ("aréa disponível");
+} else {
+    console.log ("aréa esgotada");
+}
+console.log ("fim de programa");
+
